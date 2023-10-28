8a26f393e4a03a166b2a3b1c96b423c55703d12e
damtien444 <damtien444@gmail.com>
2023-10-28 19:02:36 +0900

    doc: add readme

--- TwitchAPI/bot.py
+++ TwitchAPI/bot.py
@@ -1,5 +1,7 @@
 from typing import Callable, Union
+from twitchio.channel import Channel
 from twitchio.ext import commands
+from twitchio.user import User
 
 
 class Bot(commands.Bot):
@@ -31,7 +33,7 @@ class Bot(commands.Bot):
         # Send a hello back!
         # Sending a reply back to the channel is easy... Below is an example.
         await ctx.send(f"Hello {ctx.author.name}!")
-        
+
     async def event_message(self, message):
         # Messages with echo set to True are messages sent by the bot...
         # For now we just want to ignore them...
@@ -39,25 +41,55 @@ class Bot(commands.Bot):
             return
 
         # Print the contents of our message to console...
+        print(message)
+        print(message.author)
+        print(message.author.name)
+        print(message.timestamp)
+        print(message.tags)
+        print(message.channel)
         print(message.content)
+
+        """{
+            '@badge-info': 'predictions/NRG,founder/7', 
+            'badges': 'predictions/blue-1,founder/0', 
+            'client-nonce': '327d7fb472a789cbfeee1287765e15d8', 
+            'color': '#DAA520', 
+            'display-name': 'kamefortres', 
+            'emotes': '', 
+            'first-msg': '0', 
+            'flags': '', 
+            'id': 'e088040b-d729-4062-b3c4-c89be4da965b', 
+            'mod': '0', 
+            'returning-chatter': '0', 
+            'room-id': '104833324', 
+            'subscriber': '1', 
+            'tmi-sent-ts': '1698478538062', 
+            'turbo': '0', 
+            'user-id': '149381353', 
+            'user-type': ''
+        }"""
 
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
-
-
-if __name__=="__main__":
     
+    async def event_join(self, channel: Channel, user: User):
+        print(f"{user.name} has joined {channel.name}")
+        return await super().event_join(channel, user)
+
+
+if __name__ == "__main__":
     import os
-    
+
     # load secrets from .env file
     from dotenv import load_dotenv
+
     load_dotenv()
-    
+
     ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
     PREFIX = os.getenv("PREFIX")
     INITIAL_CHANNELS = os.getenv("INITIAL_CHANNELS").split(",")
-    
+
     # This is our main entry point into our program...
     # We create an instance of our Bot class and run it...
     bot = Bot(
