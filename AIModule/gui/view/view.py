64e30dbc495085ed66affed2d2054c8e26627c08
damtien444 <damtien444@gmail.com>
2023-12-12 17:15:10 +0900

    auto update gui after an interval

--- AIModule/gui/view/view.py
+++ AIModule/gui/view/view.py
@@ -1,56 +1,138 @@
+import json
 import tkinter as tk
-from tkinter import ttk
-
-# Main application window
-root = tk.Tk()
-root.title("Game Betting Interface")
-
-root.resizable(False, False)
-
-# Frame for the game display (assuming a label for simplicity)
-game_frame = tk.LabelFrame(root, text="Game", width=640, height=480)
-game_frame.grid(row=0, column=0, rowspan=4, columnspan=2, padx=10, pady=10)
+from tkinter import PhotoImage, ttk
 
 
-# Contact and advertisement frame
-contact_frame = tk.LabelFrame(root, text="Contact Us")
-contact_frame.grid(row=5, column=2, padx=10, pady=10)
-tk.Label(contact_frame, text="Group3").pack()
+class GameView:
+    def __init__(self, update_interval=1000):
+        # Main application window
+        self.root = tk.Tk()
+        self.root.title("Game Betting Interface")
+        self.root.resizable(False, False)
 
-ad_frame = tk.LabelFrame(root, text="Advertisement")
-ad_frame.grid(row=5, column=3, padx=10, pady=10)
-tk.Label(ad_frame, text="Ad").pack()
+        self.update_interval = update_interval
 
-# Character 1 and 2 information frames
-character1_frame = tk.LabelFrame(root, text="Character 1")
-character1_frame.grid(row=5, column=0, padx=10, pady=10)
+        # Frame for the game display (assuming a label for simplicity)
+        self.game_frame = tk.LabelFrame(self.root, text="Game", width=640, height=480)
+        self.game_frame.grid(row=0, column=0, rowspan=4, columnspan=2, padx=10, pady=10)
 
-character2_frame = tk.LabelFrame(root, text="Character 2")
+        # Contact and advertisement frame
+        self.contact_frame = tk.LabelFrame(self.root, text="Contact Us")
+        self.contact_frame.grid(row=5, column=2, padx=10, pady=10)
+        tk.Label(self.contact_frame, text="Group3").pack()
 
-character2_frame.grid(row=5, column=1, padx=10, pady=10)
+        self.ad_frame = tk.LabelFrame(self.root, text="Advertisement")
+        self.ad_frame.grid(row=5, column=3, padx=10, pady=10)
+        tk.Label(self.ad_frame, text="Ad").pack()
 
-# Placeholder for player names in character frames (using labels for simplicity)
-tk.Label(character1_frame, text="player1, player2, etc.").pack()
-tk.Label(character2_frame, text="player1, player2, etc.").pack()
+        # Character 1 and 2 information frames
+        self.character1_frame = tk.LabelFrame(self.root, text="Character 1")
+        self.character1_frame.grid(row=5, column=0, padx=10, pady=10)
 
-# Frame for balance leaderboard
+        self.character2_frame = tk.LabelFrame(self.root, text="Character 2")
+        self.character2_frame.grid(row=5, column=1, padx=10, pady=10)
 
-balance_leaderboard_frame = tk.LabelFrame(root, text="Balance Leaderboard")
-balance_leaderboard_frame.grid(row=1, column=2, columnspan=2, padx=10, pady=10)
+        # Placeholder for player names in character frames (using labels for simplicity)
+        self.player1_label = tk.Label(
+            self.character1_frame, text="player1, player2, etc."
+        )
+        self.player1_label.pack()
 
-# Example of how to add items to the balance leaderboard
-for i in range(5):
+        self.player2_label = tk.Label(
+            self.character2_frame, text="player1, player2, etc."
+        )
+        self.player2_label.pack()
 
-   ttk.Label(balance_leaderboard_frame, text=f"Player {i+1} (score)").pack()
+        # Frame for balance leaderboard
+        self.balance_leaderboard_frame = tk.LabelFrame(
+            self.root, text="Balance Leaderboard"
+        )
+        self.balance_leaderboard_frame.grid(
+            row=1, column=2, columnspan=2, padx=10, pady=10
+        )
 
-# Frame for game stats
-game_stats_frame = tk.LabelFrame(root, text="Game Stats")
-game_stats_frame.grid(row=2, column=2, columnspan=2, padx=10, pady=10)
+        # Example of how to add items to the balance leaderboard
+        # for i in range(5):
+        #     ttk.Label(self.balance_leaderboard_frame, text=f"Player {i+1} (score)").pack()
 
-# Placeholder for actual game stats (using a label for simplicity)
-tk.Label(game_stats_frame, text="Current Match: \nNext Match:").pack()
+        self.player_leaderboard_records = []
+
+        # Frame for game stats
+        self.game_stats_frame = tk.LabelFrame(self.root, text="Game Stats")
+        self.game_stats_frame.grid(row=2, column=2, columnspan=2, padx=10, pady=10)
+
+        # Placeholder for actual game stats (using a label for simplicity)
+        self.game_stats_label = tk.Label(
+            self.game_stats_frame, text="Current Match: \nNext Match:"
+        )
+        self.game_stats_label.pack()
+
+        # frameCnt = 12
+        # self.frames = [PhotoImage(file='mygif.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
+        # self.gif_label = tk.Label(self.ad_frame)
+        # self.gif_label.pack()
+        # self.root.after(0, self.update_gif, 0)
+
+        # Update the content of the LabelFrame every 1 second
+        self.root.after(self.update_interval, self.update_content)
+
+    def update_content(self):
+        status = self._read_status_json()
+        # print(status)
+
+        # destroy previous leaderboard
+        for child in self.balance_leaderboard_frame.winfo_children():
+            child.destroy()
+
+        self.player_leaderboard_records = []
+
+        # update leaderboard
+        for i in range(len(status["top_5_balance"])):
+            user_id, user_name, balance = status["top_5_balance"][i]
+            label = ttk.Label(
+                self.balance_leaderboard_frame, text=f"Player {user_name}: {balance}"
+            ).pack()
+            self.player_leaderboard_records.append(label)
+
+        player_1_list = []
+        player_2_list = []
+
+        for current_player in status["player_list"]:
+            if current_player["player_team"] == "player_1":
+                player_1_list.append(current_player["username"])
+            elif current_player["player_team"] == "player_2":
+                player_2_list.append(current_player["username"])
+
+        self.player1_label.config(text=", ".join(player_1_list))
+        self.player2_label.config(text=", ".join(player_2_list))
+
+        next_game_list = []
+
+        for next_player in status["next_game_queue"]:
+            next_game_list.append(next_player["username"])
+
+        self.game_stats_label.config(text=f"Next Match: {', '.join(next_game_list)}")
+
+        # Schedule the next update after 1 second
+        self.root.after(self.update_interval, self.update_content)
+
+    @staticmethod
+    def _read_status_json():
+        with open("status.json", "r") as f:
+            return json.load(f)
+
+    # def update_gif(self):
+    #     frame = self.frames[ind]
+    #     ind += 1
+    #     if ind == self.frameCnt:
+    #         ind = 0
+    #     self.gif_label.configure(image=frame)
+    #     self.root.after(100, self.update_gif, ind)
+
 
 if __name__ == "__main__":
-    # Run the application
-    root.mainloop()
+    # Create an instance of the GameView class
+    game_view = GameView()
 
+    # Run the application
+    game_view.root.mainloop()
